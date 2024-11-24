b307a1453cef428ebd1d4078d195c607598c91df
Evgniia <zeneka44@gmail.com>
2024-11-24 19:56:06 +0100

    Создание и удаление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узлы
+const cardsContainer = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+//_____________________
 
-// @todo: Функция удаления карточки
+// Функция создания карточки
+function createCard(initialCards, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  cardImage.src = initialCards.link;
+  cardImage.alt = initialCards.name;
+  cardTitle.textContent = initialCards.name;
+  
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteCard);
+  return cardElement;
+}
 
-// @todo: Вывести карточки на страницу
+// Функция удаления карточки
+function deleteCard(evt) {
+  const eventTarget = evt.target;
+  const cardElement = eventTarget.closest('.card');
+  cardElement.remove();
+}
+//_____________________
+
+// Вывести карточки на страницу
+initialCards.forEach(initialCards => {
+  const cardElement = createCard(initialCards, deleteCard);
+  cardsContainer.append(cardElement);
+});
