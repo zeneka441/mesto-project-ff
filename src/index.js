444b3a23d3d5870e85d6151d872c1f7622e4634c
Evgniia <zeneka44@gmail.com>
2025-04-29 23:31:39 +0200

    Клинап

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 // Подключение стилей
-import './vendor/normalize.css';
-import './vendor/fonts.css';
-import './blocks/page/page.css'; // и т.д. стили блоков
-import './pages/index.css'; // общая стилизация страницы
-
+import "./vendor/normalize.css";
+import "./vendor/fonts.css";
+import "./blocks/page/page.css";
+import "./pages/index.css";
 
 // Импорт картинок
-import logo from './images/logo.svg';
-import avatar from './images/avatar.jpg';
+import logo from "./images/logo.svg";
+import avatar from "./images/avatar.jpg";
 
 // Установка картинок через JS
-const logoImg = document.querySelector('.logo');
+const logoImg = document.querySelector(".logo");
 if (logoImg) logoImg.src = logo;
 
-const avatarImg = document.querySelector('.profile__image');
+const avatarImg = document.querySelector(".profile__image");
 if (avatarImg) avatarImg.style.backgroundImage = `url(${avatar})`;
 
 // Импорт скриптов
-import './scripts/index.js';
-import './scripts/cards.js';
+import "./scripts/index.js";
+import "./scripts/cards.js";
--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,59 @@
-// webpack.config.js
-const path = require('path'); // подключаем path к конфигу вебпак
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключите плагин
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин
-// подключите к проекту mini-css-extract-plugin
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: { main: './src/index.js' },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-                publicPath: '/'
-    },
-    mode: 'development', // добавили режим разработчика
-    // настройки локального сервера:
-    devServer: {
-        static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-        compress: true, // это ускорит загрузку в режиме разработки
-        port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-    
-        open: true // сайт будет открываться сам при запуске npm run dev
+  entry: { main: "./src/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "/",
+  },
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
       },
-      module: {
-        rules: [ // rules — это массив правил
-          // добавим в него объект правил для бабеля
-          {
-            // регулярное выражение, которое ищет все js файлы
-            test: /\.js$/,
-            // при обработке этих файлов нужно использовать babel-loader
-            use: 'babel-loader',
-            // исключает папку node_modules, файлы в ней обрабатывать не нужно
-            exclude: /node_modules/
-          },
-          {
-            // регулярное выражение, которое ищет все файлы с такими расширениями
-            test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-            type: 'asset/resource'
-          },
-          {
-            test: /\.html$/,
-            loader: 'html-loader'
-          },
-          {
-            // применять это правило только к CSS-файлам
-            test: /\.css$/,
-            // при обработке этих файлов нужно использовать
-            // MiniCssExtractPlugin.loader и css-loader
-            use: [MiniCssExtractPlugin.loader, {
-              loader: 'css-loader',
-              options: { importLoaders: 1 }
-            },
-            // Добавьте postcss-loader
-          'postcss-loader']
-          }
-          
-          ]
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
       },
-      plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-          filename: '[name].css'
-        }),
-        new HtmlWebpackPlugin({
-          template: './src/index.html',
-          inject: 'body',
-        })
-      ],
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 },
+          },
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: "body",
+    }),
+  ],
 };
-
-// переписали точку выхода, используя утилиту path
-
-// module.exports — это синтаксис экспорта в Node.js
