4f3211575180dbbeb9012fd275924a6598004ea4
Evgniia <zeneka44@gmail.com>
2025-05-08 11:44:42 +0200

    Владелец и лайк

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,4 @@
-export function createCard(cardData, deleteCard, handleLike, cardClick) {
+export function createCard(cardData, deleteCard, handleLike, cardClick, currentUserId) {
     const cardTemplate = document.querySelector("#card-template").content;
     const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   
@@ -7,15 +7,21 @@ export function createCard(cardData, deleteCard, handleLike, cardClick) {
     const deleteButton = cardElement.querySelector(".card__delete-button");
     const likeButton = cardElement.querySelector(".card__like-button");
     const likeCount = cardElement.querySelector(".card__like-counter");
+    likeCount.textContent = Array.isArray(cardData.likes) ? cardData.likes.length : 0;
+
   
     cardImage.src = cardData.link;
     cardImage.alt = cardData.name;
     cardTitle.textContent = cardData.name;
     likeCount.textContent = cardData.likes.length;
   
-    deleteButton.addEventListener("click", deleteCard);
-    // likeButton.addEventListener("click", (evt) => handleLike(evt, likeCount));
-    likeButton.addEventListener("click", handleLike);
+    // Проверяем, является ли текущий пользователь владельцем карточки
+    if (cardData.owner && cardData.owner._id === currentUserId) {
+      deleteButton.addEventListener("click", deleteCard);
+    } else {
+      deleteButton.remove();
+    }
+    likeButton.addEventListener("click", (evt) => handleLike(evt, likeCount));
     cardImage.addEventListener("click", cardClick);
   
     return cardElement;
