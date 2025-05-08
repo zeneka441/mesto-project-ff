421529ed4b2d7f7bf25c705ef85dbe73d48e0394
Evgniia <zeneka44@gmail.com>
2025-05-08 18:18:51 +0200

    Изменен publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
-    publicPath: "/",
+    publicPath: "/mesto-project-ff/",
   },
   mode: "development",
   devServer: {
