c0835f2566eef6d4b22835ce39cada6091ea883a
Evgniia <zeneka44@gmail.com>
2025-05-09 09:41:29 +0200

    Зависимости

--- webpack.config.js
+++ webpack.config.js
@@ -3,57 +3,59 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: { main: "./src/index.js" },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-    publicPath: "/mesto-project-ff/",
-  },
-  mode: "development",
-  devServer: {
-    static: path.resolve(__dirname, "./dist"),
-    compress: true,
-    port: 8080,
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
 
-    open: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: "babel-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: "asset/resource",
-      },
-      {
-        test: /\.html$/,
-        loader: "html-loader",
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: { importLoaders: 1 },
-          },
-          "postcss-loader",
-        ],
-      },
+  return {
+    entry: { main: "./src/index.js" },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "main.js",
+      publicPath: isProduction ? "/mesto-project-ff/" : "/",
+    },
+    mode: isProduction ? "production" : "development",
+    devServer: {
+      compress: true,
+      port: 8080,
+      open: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: "babel-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+          type: "asset/resource",
+        },
+        {
+          test: /\.html$/,
+          loader: "html-loader",
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: { importLoaders: 1 },
+            },
+            "postcss-loader",
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        inject: "body",
+      }),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      inject: "body",
-    }),
-  ],
+  };
 };
